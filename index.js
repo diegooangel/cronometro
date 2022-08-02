65e1f0e0a9cd2b74f64b74f62f08207f12c5930c
diegooangel <diego09.11.2000@gmail.com>
2022-08-02 11:08:59 -0300

    creamos en el archivo js la funcion del inicio del cronometro

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const $h2 = document.getElementById('h2');
-
+console.log($h2);
 let segundos = 0;
 let minutos  = 0;
 let horas    = 0;
 let t;
 
+const ticktack = () => {
+    segundos++;
+    if(segundos >= 60){
+        minutos++;
+        segundos = 0;
+        if(minutos >= 60){
+            horas++;
+            minutos = 0;
+        }
+    }
+}
+
+const agregar = () => {
+    ticktack();
+    $h2.textContent = `${horas}:${minutos}:${segundos}`
+    tiempo();
+}
+const tiempo = () => {
+    setTimeout(() => {
+        agregar();
+    }, 1000);
+}
